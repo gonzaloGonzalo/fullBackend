904ca54b9874f3a97a8b01780725f27dd654ff13
agustincba <agustin.carban@gmail.com>
2017-03-22 15:25:21 -0300

    REST Service Method To Query A Single Employee

--- src/main/java/ar/com/fullbackend/service/EmployeeServiceImpl.java
+++ src/main/java/ar/com/fullbackend/service/EmployeeServiceImpl.java
@@ -1,14 +1,14 @@
 package ar.com.fullbackend.service;
 
-import ar.com.fullbackend.dao.impl.EmployeeDAO;
-import ar.com.fullbackend.dto.Employee;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+
+import ar.com.fullbackend.dao.impl.EmployeeDAO;
+import ar.com.fullbackend.dto.Employee;
 
 /**
  * Created by computer on 20/03/17.
@@ -25,7 +25,20 @@ public class EmployeeServiceImpl{
     }
 
     @GET
+    @Path("/single/{employeeId: [0-9]}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response obtainEmployee(@PathParam("employeeId") Integer employeeId) {
+        Employee employee = new Employee();
+        try {
+            return Response.ok(employee).build();
+        } catch (Exception ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GET
     @Path("/status")
     public String getStatus(){
         return "App is alive";
-    }}
+    }
+}
